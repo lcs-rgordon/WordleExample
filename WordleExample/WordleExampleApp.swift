1650406c87e0cd0c1a7f3c6b67bbcf4abe1d30da
Russell Gordon <rgordon@lcs.on.ca>
2026-05-19 14:24:30 -0400

    Initial Commit

---
+++ WordleExample/WordleExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleExampleApp.swift
+//  WordleExample
+//
+//  Created by Russell Gordon on 2026-05-19.
+//
+
+import SwiftUI
+
+@main
+struct WordleExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HousesListView()
+        }
+    }
+}
